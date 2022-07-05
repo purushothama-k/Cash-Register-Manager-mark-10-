8ee1c816f200af595ec0c8ede311b59472840295
Purushothama <purushothamak269@gmail.com>
2022-07-05 22:43:22 +0530

    Update: JS changes on amount to be returned value.

--- JS/app.js
+++ JS/app.js
@@ -5,16 +5,24 @@ const cashGivenEl = document.querySelector("#cash-given");
 const calculateBtnEl = document.querySelector("#calculate-btn");
 const errorMessageEl = document.querySelector("#error-msg");
 
-// errorMessageEl.textContent = "";
+const notes = [2000, 500, 100, 20, 10, 5, 1];
 
 calculateBtnEl.addEventListener("click", function () {
   errorMessageEl.textContent = "";
   if (billAmountEl.value > 0) {
-    if (cashGivenEl.value >= billAmountEl) {
+    if (cashGivenEl.value >= billAmountEl.value) {
+      const amountToBereturned = cashGivenEl.value - billAmountEl.value;
+      console.log(amountToBereturned);
     } else {
-      errorMessageEl.textContent = "Do you wanna wash plates 🍽?";
+      errorMessageEl.textContent = "Do you want to work with us 😃 🍽?";
     }
   } else {
     errorMessageEl.textContent = "Please enter a valid amount";
   }
 });
+
+const validatingTheChange = function (amountToBereturned) {
+  for (let i = 0; i < notes.length; i++) {
+    const numberOfNotesToBeReturned = Math.trunc(amountToBereturned / notes[i]);
+  }
+};
