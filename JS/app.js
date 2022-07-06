f68b962bc195cd5e58ecd3caddb12cb361b26d6b
Purushothama <purushothamak269@gmail.com>
2022-07-06 23:29:37 +0530

    Done

--- JS/app.js
+++ JS/app.js
@@ -1,23 +1,23 @@
-'use strict';
+"use strict";
 
-const billAmountEl = document.querySelector('#bill-amount');
-const cashGivenEl = document.querySelector('#cash-given');
-const calculateBtnEl = document.querySelector('#calculate-btn');
-const errorMessageEl = document.querySelector('#error-msg');
-const numberOfNotes = document.querySelectorAll('.notesChange');
+const billAmountEl = document.querySelector("#bill-amount");
+const cashGivenEl = document.querySelector("#cash-given");
+const calculateBtnEl = document.querySelector("#calculate-btn");
+const errorMessageEl = document.querySelector("#error-msg");
+const numberOfNotes = document.querySelectorAll(".notesChange");
 const notes = [2000, 500, 100, 20, 10, 5, 1];
 
-calculateBtnEl.addEventListener('click', function () {
-  errorMessageEl.textContent = '';
+calculateBtnEl.addEventListener("click", function () {
+  errorMessageEl.textContent = "";
   if (Number(billAmountEl.value) > 0 || Number(cashGivenEl.value) > 0) {
     if (Number(cashGivenEl.value) >= Number(billAmountEl.value)) {
       const amountToBeReturned = cashGivenEl.value - billAmountEl.value;
       validatingTheChange(amountToBeReturned);
     } else {
-      errorMessageEl.textContent = 'Do you want to work with us 😃 🍽 ?';
+      errorMessageEl.textContent = "Do you want to work with us 😃 🍽 ?";
     }
   } else {
-    errorMessageEl.textContent = 'Please enter a valid amount';
+    errorMessageEl.textContent = "Please enter a valid amount";
   }
 });
 
